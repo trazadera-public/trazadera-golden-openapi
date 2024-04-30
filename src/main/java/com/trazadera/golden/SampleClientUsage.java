fea8637c21e5f9c95f97749c2bb2e2abadb6b8c5
nlafuente-trazadera <nlafuente@trazadera.com>
2024-04-30 17:11:30 +0200

    added sample usage

---
+++ src/main/java/com/trazadera/golden/SampleClientUsage.java
@@ -0,0 +1,28 @@
+package com.trazadera.golden;
+
+import com.trazadera.golden.restclient.api.EntityApi;
+import com.trazadera.golden.restclient.invoker.ApiClient;
+import com.trazadera.golden.restclient.model.EntityListResponseDto;
+
+public class SampleClientUsage {
+
+    public static void main(String[] args) throws Exception {
+        // Required to initialize the API client
+        String url = "https://foo.trazadera.net";
+        String token = "golden:XXXX";
+
+        // Initialize API client
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(url);
+        apiClient.setAccessToken(token);
+
+        // Select the API module
+        EntityApi api = new EntityApi();
+        api.setApiClient(apiClient);
+
+        // Use the API to list all entities
+        EntityListResponseDto entities = api.getEntities();
+        System.out.println(entities);
+    }
+
+}
