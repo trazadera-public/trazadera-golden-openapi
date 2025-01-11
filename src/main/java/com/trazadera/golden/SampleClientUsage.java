06f2d2ff932150c870d676ca91d6bfb2a9e160cd
nlafuente-trazadera <nlafuente@trazadera.com>
2025-01-11 20:39:07 +0100

    Version 16-SNAPSHOT

--- src/main/java/com/trazadera/golden/SampleClientUsage.java
+++ src/main/java/com/trazadera/golden/SampleClientUsage.java
@@ -1,28 +1,50 @@
 package com.trazadera.golden;
 
+import com.trazadera.golden.restclient.api.ConfigurationApi;
 import com.trazadera.golden.restclient.api.EntityApi;
 import com.trazadera.golden.restclient.invoker.ApiClient;
+import com.trazadera.golden.restclient.invoker.ApiException;
+import com.trazadera.golden.restclient.model.ConfigurationVersionResponseDto;
+import com.trazadera.golden.restclient.model.EntityDto;
 import com.trazadera.golden.restclient.model.EntityListResponseDto;
 
 public class SampleClientUsage {
 
-    public static void main(String[] args) throws Exception {
-        // Required to initialize the API client
-        String url = "https://foo.trazadera.net";
-        String token = "golden:XXXX";
+    public static void main(String[] args) {
+        try {
+            // Required to initialize the API client
+            if (args.length < 2) {
+                System.out.println("Usage: SampleClientUsage <url> <token>");
+                System.exit(1);
+            }
+            String url = args[0];
+            String token = args[1];
 
-        // Initialize API client
-        ApiClient apiClient = new ApiClient();
-        apiClient.setBasePath(url);
-        apiClient.setAccessToken(token);
+            // Initialize API client
+            ApiClient apiClient = new ApiClient();
+            apiClient.setBasePath(url);
+            apiClient.setAccessToken(token);
 
-        // Select the API module
-        EntityApi api = new EntityApi();
-        api.setApiClient(apiClient);
+            // Get product version
+            ConfigurationApi configurationApi = new ConfigurationApi(apiClient);
+            ConfigurationVersionResponseDto version = configurationApi.getVersion();
+            System.out.println("Product version: " + version.getVersion());
 
-        // Use the API to list all entities
-        EntityListResponseDto entities = api.getAllEntities();
-        System.out.println(entities);
+            // List all entities
+            EntityApi entityApi = new EntityApi(apiClient);
+            EntityListResponseDto entities = entityApi.getAllEntities();
+            System.out.println("Total entities: " + entities.getEntities().size());
+            System.out.println("Entities: ");
+            for (EntityDto entity : entities.getEntities()) {
+                System.out.println("\t" + entity.getId() + ": " + entity.getDescription());
+            }
+
+        } catch (ApiException e) {
+            System.out.println("An API error occurred: " + e.getResponseBody());
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 }
